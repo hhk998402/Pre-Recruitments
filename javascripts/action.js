cc648a09f9d5011948bd3460f02499c5bb83ab07
hhk998402 <hemant.hari.kumar@gmail.com>
2018-11-30 05:09:45 +0530

    Captcha Refresh Fix

--- javascripts/action.js
+++ javascripts/action.js
@@ -3,9 +3,52 @@ $(document).ready(function() {
         var d = new Date();
         var n = d.getMilliseconds();
         console.log("HERE");
-        var link = "http://prerecruitments-backend.herokuapp.com/captcha/captchagen" + "?param1=" + n.toString();
+        var link = "https://prerecruitments-backend.herokuapp.com/captcha/captchagen" + "?param1=" + n.toString();
         document.getElementById("captchaImg").src=link;
     }
+
+    $("#regBtn").click(function(e) {
+        e.preventDefault();
+        var name = $("#name").val();
+        var email = $("#email").val();
+        var phone = $("#phone").val();
+        var regno = $("#regno").val();
+        var captchaValue = $("#captchaVal").val();
+        var Tech = $("#Tech").val();
+        var Management = $("#Management").val();
+        var Design = $("#Design").val();
+        var data = JSON.stringify({
+            name: name,
+            email : email,
+            phone : phone,
+            regno : regno,
+            captchaValue : captchaValue,
+            Tech : Tech,
+            Management : Management,
+            Design : Design
+        });
+        $.ajax({
+            url: 'https://prerecruitments-backend.herokuapp.com/user/create',
+            type: 'post',
+            dataType: 'json',
+            contentType: 'application/json',
+            success: function (data) {
+                console.log(data);
+            },
+            data: data
+        });
+
+
+
+        // if(!name || name === ''){
+        //     $('#name').css('border', '1px solid red');
+        //     $('#nameErr').text("This field is Mandatory");
+        //     $('#nameErr').show();
+        // }
+        // else if(){
+        //
+        // }
+    });
 
     captchaGen();
 
